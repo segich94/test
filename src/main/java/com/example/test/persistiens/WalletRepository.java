d64ad98534920b75cbc18abada1dc3674ad5b09b
Sergey Strunin <sstrunin@openintegration.inc>
2024-04-09 16:11:04 +0300

    pissimistic lock

--- src/main/java/com/example/test/persistiens/WalletRepository.java
+++ src/main/java/com/example/test/persistiens/WalletRepository.java
@@ -1,10 +1,13 @@
 package com.example.test.persistiens;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Wallet> findWalletByUuid(UUID uuid);
 }
